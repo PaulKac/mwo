9fa6ae0f701632226cd66418325b8dcb6a6cebc0
Shadov <shadov.profile@gmail.com>
2017-12-05 14:54:17 +0100

    Zmiana strony glownej (merge konflikty?)

--- Klasterix/klasterix-frontend/src/main/app/components/home.js
+++ Klasterix/klasterix-frontend/src/main/app/components/home.js
@@ -36,22 +36,9 @@ class Home extends React.Component {
         }
 
         return (
-
             <Wrapper>
                 <Title>
                     Welcome to Klasterix! Enjoy.
-
-                    <form style={{maxWidth: 350}} className="form-signin mx-auto" method="POST" action="../loginToUser">
-                        <h2 className="form-signin-heading">Logowanie</h2>
-                        <label htmlFor="input" className="sr-only">Nazwa</label>
-                        <input type="text" id="inputLogin" className="form-control p-6" placeholder="Nazwa użytkownika"
-                               required autoFocus name="login"/>
-                        <label htmlFor="inputPassword" className="sr-only">Hasło</label>
-                        <input type="password" id="inputPassword" className="form-control mb-3 p-6" placeholder="Password"
-                               required name="pass"/>
-                        <button className="btn btn-primary btn-block" type="submit">Zaloguj</button>
-                    </form>
-
                 </Title>
                 {loginStatus}
             </Wrapper>
