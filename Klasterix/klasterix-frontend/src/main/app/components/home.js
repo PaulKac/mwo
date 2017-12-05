d07a80ae1099c95441f14809a1b58252d409048f
PaulKac <xpikayx@gmail.com>
2017-12-05 13:48:32 +0100

    front logowania

--- Klasterix/klasterix-frontend/src/main/app/components/home.js
+++ Klasterix/klasterix-frontend/src/main/app/components/home.js
@@ -15,9 +15,22 @@ const Wrapper = styled.section`
 class Home extends React.Component {
     render() {
         return (
+
             <Wrapper>
                 <Title>
                     Welcome to Klasterix! Enjoy.
+
+                    <form style={{maxWidth: 350}} className="form-signin mx-auto" method="POST" action="../loginToUser">
+                        <h2 className="form-signin-heading">Logowanie</h2>
+                        <label htmlFor="input" className="sr-only">Nazwa</label>
+                        <input type="text" id="inputLogin" className="form-control p-6" placeholder="Nazwa użytkownika"
+                               required autoFocus name="login"/>
+                        <label htmlFor="inputPassword" className="sr-only">Hasło</label>
+                        <input type="password" id="inputPassword" className="form-control mb-3 p-6" placeholder="Password"
+                               required name="pass"/>
+                        <button className="btn btn-primary btn-block" type="submit">Zaloguj</button>
+                    </form>
+
                 </Title>
             </Wrapper>
         )
